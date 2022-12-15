ba0488110bb6652504a7cec60d42deb568513d87
Andrei0205 <andyr3.ae@gmail.com>
2022-12-15 18:51:45 +0200

    All declarations in main()

--- Week1_p4/src/main/java/com/nagarro/remotelearning/main/Main.java
+++ Week1_p4/src/main/java/com/nagarro/remotelearning/main/Main.java
@@ -4,9 +4,9 @@ package com.nagarro.remotelearning.main;
 import com.nagarro.remotelearning.app.ConnectionsManager;
 
 public class Main {
-   static ConnectionsManager connectionsManager = new ConnectionsManager();
 
     public static void main(String[] args) {
+        ConnectionsManager connectionsManager = new ConnectionsManager();
         System.out.println(connectionsManager.getConnection(2));
         System.out.println(connectionsManager.getConnection(21));
 
