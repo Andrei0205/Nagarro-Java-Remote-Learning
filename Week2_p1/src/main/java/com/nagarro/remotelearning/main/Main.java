d417bf9910631c485ca47fe3e3dd2ebd845c0f79
Andrei0205 <andyr3.ae@gmail.com>
2022-12-15 18:45:42 +0200

    Made "firstName" as List of names.

--- Week2_p1/src/main/java/com/nagarro/remotelearning/main/Main.java
+++ Week2_p1/src/main/java/com/nagarro/remotelearning/main/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Person person1 = new Person("Jhon", "Rhon");
         Person person2 = new Person("Thom Ferris Moris");
+        Person person3 = new Person("Thom Ferris Moris Boris");
 
         System.out.println(person1);
         System.out.println(person2);
+        System.out.println(person3);
     }
 
 }
--- Week2_p1/src/main/java/com/nagarro/remotelearning/model/Person.java
+++ Week2_p1/src/main/java/com/nagarro/remotelearning/model/Person.java
@@ -1,29 +1,36 @@
 package com.nagarro.remotelearning.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Person {
-    public static final int SURNAME_INDEX = 0;
-    public static final int FIRSTNAME_INDEX = 1;
+    private static final int SURNAME_INDEX = 0;
+    private int startIndex = 0;
     private String surname;
-    private String firstname;
+    private List<String> firstname = new ArrayList<>();
 
     public Person(String surname, String firstname) {
         this.surname = surname;
-        this.firstname = firstname;
+        assignFirstNames(splitName(firstname));
     }
 
     public Person(String fullname) {
-        String[] splittedNames = splitName(fullname);
-        assignNames(splittedNames);
+        List<String> splittedNames = splitName(fullname);
+        this.surname = splittedNames.get(SURNAME_INDEX);
+        startIndex += 1;
+        assignFirstNames(splittedNames);
     }
 
-    private String[] splitName(String fullname) {
-        String[] splittedString = fullname.split(" ", 2);
+    private List<String> splitName(String fullname) {
+        List<String> splittedString = Arrays.asList(fullname.split(" "));
         return splittedString;
     }
 
-    private void assignNames(String[] fullnameSplitted) {
-        this.surname = fullnameSplitted[SURNAME_INDEX];
-        this.firstname = fullnameSplitted[FIRSTNAME_INDEX];
+    private void assignFirstNames(List<String> fullnameSplitted) {
+        for (int i = startIndex; i < fullnameSplitted.size(); i++) {
+            firstname.add(fullnameSplitted.get(i));
+        }
     }
 
     @Override
