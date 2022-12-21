8395eb4da7234f761940b5f15429bbf7065317a4
Andrei0205 <andyr3.ae@gmail.com>
2022-12-21 16:40:41 +0200

    Total refactoring + added LocalDate

--- Week3_p1/src/main/java/com/nagarro/remotelearning/app/English.java
+++ Week3_p1/src/main/java/com/nagarro/remotelearning/app/English.java
@@ -2,73 +2,30 @@ package com.nagarro.remotelearning.app;
 
 import com.nagarro.remotelearning.model.Person;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 public class English extends Person {
-    private static final int CURRENT_YEAR = 2022;
-    private static final int SURNAME_INDEX = 0;
-    private static final int BIRTH_DAY_INDEX = 0;
-    private static final int BIRTH_MONTH_INDEX = 1;
-    private static final int BIRTH_YEAR_INDEX = 2;
-    private int startIndex = 0;
-
-    private String surname;
-    private List<String> firstname = new ArrayList<>();
-    private String birthDate;
-
-    private int birthDay;
-    private int birthMonth;
-    private int birthYear;
-
-
     public English(String surname, String firstname, String birthDate) {
-        this.surname = surname;
-        assignFirstNames(splitName(firstname));
-        this.birthDate = birthDate;
+        super(surname, firstname);
+        this.birthDate = LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     public English(String fullname, String birthDate) {
-        List<String> splittedNames = splitName(fullname);
-        this.surname = splittedNames.get(SURNAME_INDEX);
-        startIndex += 1;
-        assignFirstNames(splittedNames);
-        this.birthDate = birthDate;
+        super(fullname);
+        this.birthDate = LocalDate.parse(birthDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     }
 
     @Override
-    public String getBirthDate() {
+    public LocalDate getBirthDate() {
         return birthDate;
     }
 
     @Override
     public void selfDescribe() {
-        extractBirthDateIntoFields(birthDate);
         String age = calculateAge();
         System.out.println("Hello,I'm from England and I like to read. I am " + age + " years old");
     }
 
-    private void extractBirthDateIntoFields(String birthDate) {
-        String[] birthDateSplitted = birthDate.split("/");
-        birthDay = Integer.parseInt(birthDateSplitted[BIRTH_DAY_INDEX]);
-        birthMonth = Integer.parseInt(birthDateSplitted[BIRTH_MONTH_INDEX]);
-        birthYear = Integer.parseInt(birthDateSplitted[BIRTH_YEAR_INDEX]);
-    }
-
-    private String calculateAge() {
-        int age = CURRENT_YEAR - birthYear;
-        return String.valueOf(age);
-    }
-
-    private List<String> splitName(String fullname) {
-        List<String> splittedString = Arrays.asList(fullname.split(" "));
-        return splittedString;
-    }
-
-    private void assignFirstNames(List<String> fullnameSplitted) {
-        for (int i = startIndex; i < fullnameSplitted.size(); i++) {
-            firstname.add(fullnameSplitted.get(i));
-        }
-    }
 }
--- Week3_p1/src/main/java/com/nagarro/remotelearning/main/Main.java
+++ Week3_p1/src/main/java/com/nagarro/remotelearning/main/Main.java
@@ -4,12 +4,13 @@ import com.nagarro.remotelearning.app.English;
 import com.nagarro.remotelearning.app.Romanian;
 import com.nagarro.remotelearning.model.Person;
 
+
 public class Main {
     public static void main(String[] args) {
-        Person romanian = new Romanian("Marinescu Andrei", "20/01/2000");
-        Person english = new English("Thomas Edison","14/11/1990");
+        Person romanian = new Romanian("Marinescu", "Andrei Ionut ", "20-01-1992");
+        Person english = new English("Jhon Ferris Anton ", "2001-02-05");
         english.selfDescribe();
         romanian.selfDescribe();
-
+        System.out.println(romanian.getBirthDate());
     }
 }
