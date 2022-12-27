5fc6c2ab67687ec6f8df86a45f84efdd34adda92
Andrei0205 <andyr3.ae@gmail.com>
2022-12-27 16:45:12 +0200

    Week4_p4 solved

---
+++ Week4_p4/src/main/java/com/nagarro/remotelearning/main/Main.java
@@ -0,0 +1,16 @@
+package com.nagarro.remotelearning.main;
+
+import com.nagarro.remotelearning.utils.PigLatinEncoder;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        PigLatinEncoder pigLatinEncoder = new PigLatinEncoder();
+        while (true) {
+            System.out.println(pigLatinEncoder.encode(scanner.nextLine()));
+        }
+    }
+}
---
+++ Week4_p4/src/main/java/com/nagarro/remotelearning/utils/PigLatinEncoder.java
@@ -0,0 +1,28 @@
+package com.nagarro.remotelearning.utils;
+
+public class PigLatinEncoder {
+
+    private static final String PIG_LATIN_SUFFIX = "ay";
+    private static final String SPACE_SEPARATOR = " ";
+    private static final int SUBSTRING_INDEX = 1;
+    private static final int FIRST_LETTER_INDEX = 0;
+    private static final String BLANK_SPACE = " ";
+
+    public String encode(String string) {
+        String[] splittedString = string.split(SPACE_SEPARATOR);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : splittedString) {
+            stringBuilder.append(transformWordIntoPigLatin(s));
+            stringBuilder.append(BLANK_SPACE);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String transformWordIntoPigLatin(String word) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(word.substring(SUBSTRING_INDEX));
+        stringBuilder.append(word.charAt(FIRST_LETTER_INDEX));
+        stringBuilder.append(PIG_LATIN_SUFFIX);
+        return stringBuilder.toString();
+    }
+}
