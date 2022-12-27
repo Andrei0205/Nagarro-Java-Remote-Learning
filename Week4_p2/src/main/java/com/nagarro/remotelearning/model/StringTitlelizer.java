47b66269a535edd8b8c395ec38191ab0e4eff892
Andrei0205 <andyr3.ae@gmail.com>
2022-12-27 14:41:33 +0200

    Week4_p2 minor adjustments

--- Week4_p2/src/main/java/com/nagarro/remotelearning/model/StringTitlelizer.java
+++ Week4_p2/src/main/java/com/nagarro/remotelearning/model/StringTitlelizer.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 public class StringTitlelizer implements Titlelizer {
 
+    private static final int FIRST_LETTER_INDEX = 0;
+    private static final int START_INDEX = 0;
+    private static final String BLANK_SPACE = " ";
+    private static final int FIRST_WORD_INDEX = 0;
     private static final List<String> IGNORED_WORDS = new ArrayList<>(Arrays.asList("the", "this", "a", "to", "in", "of", "is"));
 
     @Override
@@ -13,28 +17,28 @@ public class StringTitlelizer implements Titlelizer {
         if (toTitlelize == null) {
             throw new IllegalArgumentException();
         } else if (toTitlelize.equals("")) {
-            System.out.println("Empty string,please enter a valid one");
             return "";
-        } //
-        String[] splittedString = toTitlelize.split(" ");
+        }
+        String[] splittedString = toTitlelize.split(BLANK_SPACE);
         return capitalizeFirstLetterOfEachWord(splittedString);
     }
 
     private String capitalizeFirstLetterOfEachWord(String[] splittedString) {
         StringBuilder newString = new StringBuilder();
-        for (int i = 0; i < splittedString.length; i++) {
-            if (isNotIgnoredWord(splittedString[i]) || i == 0) {
-                newString.append(Character.toUpperCase(splittedString[i].charAt(0))).append(splittedString[i].substring(1)).append(" ");
+        for (int i = START_INDEX; i < splittedString.length; i++) {
+            if (isNotIgnoredWord(splittedString[i]) || i == FIRST_WORD_INDEX) {
+                newString.append(Character.toUpperCase(splittedString[i].charAt(FIRST_LETTER_INDEX))).append(splittedString[i].substring(1)).append(BLANK_SPACE);
             } else {
-                newString.append(splittedString[i]).append(" ");
+                newString.append(splittedString[i]).append(BLANK_SPACE);
             }
         }
         newString = newString.deleteCharAt(newString.length() - 1);
         return newString.toString();
     }
 
+
     private boolean isNotIgnoredWord(String word) {
-        for (String s : ignoredWords) {
+        for (String s : IGNORED_WORDS) {
             if (word.equals(s)) {
                 return false;
             }
