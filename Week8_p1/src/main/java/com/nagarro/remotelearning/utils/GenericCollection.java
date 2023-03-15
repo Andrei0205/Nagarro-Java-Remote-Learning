02e6e9fdd8ee474f9d24c24a82b6e88cf2d86a47
Andrei0205 <andyr3.ae@gmail.com>
2023-03-15 20:24:29 +0200

    Week8_p1 refactor

--- Week8_p1/src/main/java/com/nagarro/remotelearning/utils/GenericCollection.java
+++ Week8_p1/src/main/java/com/nagarro/remotelearning/utils/GenericCollection.java
@@ -3,18 +3,35 @@ package com.nagarro.remotelearning.utils;
 import java.util.Collection;
 
 public class GenericCollection<T> implements MyCollection{
-    private Collection<T> myCollection;
+    private final Collection<T> myCollection;
+    private T[] elements;
     public GenericCollection(Collection<T> collection) {
         this.myCollection = collection;
+        elements = myCollection.toArray((T[]) new Comparable[myCollection.size()]);
     }
+
 
     @Override
     public boolean containsAll(Collection collection) {
-        return myCollection.containsAll(collection);
+        boolean isEqual = false;
+        for (Object item : collection) {
+            isEqual = false;
+            for(T element : elements) {
+                if(item.equals(element)){
+                    isEqual =true;
+                }
+            }
+            if(!isEqual) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection collection) {
-        return myCollection.addAll(collection);
+        myCollection.addAll(collection);
+        elements = myCollection.toArray((T[]) new Comparable[myCollection.size()]);
+        return true;
     }
 }
--- Week8_p1/src/main/java/com/nagarro/remotelearning/utils/MyCollection.java
+++ Week8_p1/src/main/java/com/nagarro/remotelearning/utils/MyCollection.java
@@ -2,7 +2,8 @@ package com.nagarro.remotelearning.utils;
 
 import java.util.Collection;
 
-public interface MyCollection <T extends Collection> {
-    public  boolean containsAll(T collection);
-    public boolean addAll(T collection);
+public interface MyCollection<T extends Collection> {
+    boolean containsAll(T collection);
+
+    boolean addAll(T collection);
 }
