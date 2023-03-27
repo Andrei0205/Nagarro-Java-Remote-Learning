5e997596dcd067818f051c1fb4d3fe171ca77575
Andrei0205 <andyr3.ae@gmail.com>
2023-03-27 18:05:05 +0300

    Week10_p2

---
+++ Week10_p2/src/main/java/com/nagarro/remotelearning/utils/FileArchiver.java
@@ -0,0 +1,30 @@
+package com.nagarro.remotelearning.utils;
+
+import com.nagarro.remotelearning.main.Main;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.zip.GZIPOutputStream;
+
+public class FileArchiver {
+
+    public void readAndArchive(String source, String destination) throws IOException, URISyntaxException {
+        BufferedReader reader = new BufferedReader(
+                new FileReader(getPathFromResource(source)));
+        BufferedOutputStream writter = new BufferedOutputStream(
+                new GZIPOutputStream(new FileOutputStream(getPathFromResource(destination))));
+        int element;
+        while ((element = reader.read()) != -1)
+            writter.write(element);
+        reader.close();
+        writter.close();
+    }
+
+    private String getPathFromResource(String fileName) throws URISyntaxException {
+        URL res = Main.class.getClassLoader().getResource(fileName);
+        File file = Paths.get(res.toURI()).toFile();
+        return file.getAbsolutePath();
+    }
+}
---
+++ Week10_p2/src/main/java/com/nagarro/remotelearning/utils/FileInformer.java
@@ -0,0 +1,28 @@
+package com.nagarro.remotelearning.utils;
+
+import com.nagarro.remotelearning.main.Main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileInformer {
+    public double getFileSize(String fileName) throws URISyntaxException {
+        Path path = Paths.get(getUrlFromResourceFile(fileName));
+        double bytes = 0;
+        try {
+            bytes = Files.size(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bytes;
+    }
+
+    private URI getUrlFromResourceFile(String fileName) throws URISyntaxException {
+        return Main.class.getClassLoader().getResource(fileName).toURI();
+
+    }
+}
