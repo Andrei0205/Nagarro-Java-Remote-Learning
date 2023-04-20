eea1fa2366f8d562bf9d3ee882e0ad135730d315
Andrei0205 <andyr3.ae@gmail.com>
2023-04-20 17:08:53 +0300

    Week12_p3 refactor

--- Week12_p3/src/main/java/com/nagarro/remotelearning/main/Main.java
+++ Week12_p3/src/main/java/com/nagarro/remotelearning/main/Main.java
@@ -1,11 +1,6 @@
 package com.nagarro.remotelearning.main;
 
-import com.nagarro.remotelearning.utils.MyLock;
-
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
     public static void main(String[] args) {
-        MyLock.withLock(new ReentrantLock(),() -> System.out.println("Hello"));
     }
 }
--- Week12_p3/src/main/java/com/nagarro/remotelearning/utils/MyLock.java
+++ Week12_p3/src/main/java/com/nagarro/remotelearning/utils/MyLock.java
@@ -3,12 +3,12 @@ package com.nagarro.remotelearning.utils;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MyLock {
-    public static void withLock(ReentrantLock lock, Runnable runnable) {
+    public void withLock(ReentrantLock lock, Runnable runnable) {
         lock.lock();
         try {
             runnable.run();
         } finally {
-        lock.unlock();
+            lock.unlock();
         }
     }
 }
